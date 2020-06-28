6a0650d38b10a5543bfa3995550f7d859b8b2f85
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-06-28 13:53:51 -0600

    assignments

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 4;
+            //Console.WriteLine("Your favorite number plus 4 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursworked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.432355334;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "myron";
+
+            int currentAge = 27;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+             
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+
+        }
+    }
+}
