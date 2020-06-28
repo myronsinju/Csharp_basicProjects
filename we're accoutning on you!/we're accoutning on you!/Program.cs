d8f4669565e63158ec7d7a06aedc26ee3d30e02d
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-06-28 12:30:04 -0600

    assignment

---
+++ we're accoutning on you!/we're accoutning on you!/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace we_re_accoutning_on_you_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welecome to Acme Accounting Systems");
+            Console.WriteLine("we're 'accoutning' on you!");
+            Console.ReadLine();
+        }
+    }
+}
