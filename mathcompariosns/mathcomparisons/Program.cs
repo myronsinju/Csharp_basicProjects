a9f4483bc71854500474db42ec84c7c1d2179d37
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-06-28 17:46:20 -0600

    assignment

---
+++ mathcompariosns/mathcomparisons/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathcomparisons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    int total = 5 + 10;
+            //    int otherTotal = 12 + 22;
+            //    int combined = total + otherTotal; 
+            //    Console.WriteLine(combined);
+            //    Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Five minus Ten = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int quotient = 100 / 5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 70;
+
+            ////bool isWarm = currentTemperature >= roomTemperature;
+            //bool isWarm = currentTemperature != roomTemperature;
+
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            long product = 100 * 50;
+            Console.WriteLine(product);
+            Console.ReadLine();
+
+            int total = 10 + 25;
+            Console.WriteLine(total);
+            Console.ReadLine();
+
+            int double = 38 / 12.5;
+            Console.WriteLine(double);
+            Console.ReadLine();
+
+            int roomTemperature = 50;
+            int currentTemperature = 50;
+
+            bool isWarm = currentTemperature != roomTemperature;
+        }
+    }
+}
