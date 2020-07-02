3f4db645812920217c873b0f7feb2a80204c949a
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-07-02 17:26:03 -0600

    assignments

---
+++ Math_operations_88_solution/Math_operations_88/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_operations_88
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please input a number to multiply by 50");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int result = userInput * 50;
+            Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+//1. Takes an input from the user, multiplies it by 50, then prints the result to the console. 
+//(Note: make sure your code can take inputs larger than 10,000,000).
+
+//2. Takes an input from the user, adds 25 to it, then prints the result to the console.
+
+//3. Takes an input from the user, divides it by 12.5, then prints the result to the console.
+
+//4. Takes an input from the user, checks if it is greater than 50, then prints the true/false result to the console.
+
+//5. Takes an input from the user, divides it by 7, then prints the remainder to the console (tip: think % operator).
--- we're accoutning on you!/we're accoutning on you!/Program.cs
+++ we're accoutning on you!/we're accoutning on you!/Program.cs
@@ -6,8 +6,7 @@ namespace we_re_accoutning_on_you_
     {
         static void Main()
         {
-            Console.WriteLine("Welecome to Acme Accounting Systems");
-            Console.WriteLine("we're 'accoutning' on you!");
+            Console.WriteLine("Welecome to Acme Accounting Systems \nwe're 'accoutning on you!");
             Console.ReadLine();
         }
     }
