cf63cc4fecb338647841acac1364289fb6ef5a99
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-07-08 14:13:55 -0600

    math operatorss

--- Math_operations_88_solution/Math_operations_88/Program.cs
+++ Math_operations_88_solution/Math_operations_88/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,19 +17,30 @@ namespace Math_operations_88
             int result = userInput * 50;
             Console.WriteLine(result);
 
+            Console.WriteLine("Please input a number to add by 25");
+            int userInputadd = Convert.ToInt32(Console.ReadLine());
+            int addResults = userInputadd + 25;
+            Console.WriteLine(addResults);
+
+            Console.WriteLine("Please input a number to divide by 12.5");
+            double  A = 12.5;
+            int doubleDivide = Convert.ToInt32(Console.ReadLine());
+            int divideResult = (int)A;
+            Console.WriteLine(divideResult);
+
+
+
+            Console.WriteLine("Please enter a number to check if it is greater than 50");
+            bool greaterthan = num1 > num2
+                num1 = Any; num 2 = 50;
 
 
 
 
-
-
-
-
-
-
-
-
-
+            Console.WriteLine("please enter a number to divide by 7");
+            int quotient = Convert.ToInt32(Console.ReadLine());
+            int quotientResults = quotient / 7;
+            Console.WriteLine(quotientResults);
 
 
 
