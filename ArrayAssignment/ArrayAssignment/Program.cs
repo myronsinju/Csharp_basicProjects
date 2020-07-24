db47c6cfd730857776b546248756cfe2f0816fa7
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-07-24 01:51:24 -0600

    arrays

---
+++ ArrayAssignment/ArrayAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ArrayAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] phrases = new string[] { "what are you up to?", "How is the day?", "Is it hot outside?", "Are you a fan of cheese?",
+            "whats new scooby-doo?"};
+            Console.WriteLine("please choose a index number between 0-4");
+
+            Console.WriteLine(phrases);
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(phrases[input]);
+            Console.ReadLine();
+
+            int[] numArray = new int[] { 7, 14, 3300, 29, 85, 3 };
+            Console.WriteLine("please select an index number of the array");
+            Console.WriteLine(numArray);
+            int input2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(numArray[input2]);
+        Retrypoint:
+            int a = int.Parse(Console.ReadLine());
+            if ((a > 0) && (a <= 5));
+            else
+            {
+                Console.WriteLine("the number you selected of the array does not exist please enter a whole number less than 5 ");
+                goto Retrypoint;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+////int[] numArray1 = new int[] { 5, 10, 546, 1525, 2368 };
+
+//Console.WriteLine(cisTuition[index]);
+
+//int input = int.Parse(Console.ReadLine());
+//Console.WriteLine("value is: " + cisTuition[input]);
---
+++ ArraysList/ArraysList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        List<string> intList = new List<string>();
+        intList.Add("Hiya");
+        intList.Add("Dmy");
+        intList.Remove("Dmy");
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+
+
+
+
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 10;
+        //numArray[2] = 546;
+        //numArray[3] = 1525;
+        //numArray[4] = 2368;
+
+        //int[] numArray1 = new int[] { 5, 10, 546, 1525, 2368 };
+
+        //int[] numArray2 = { 2, 6, 1, 105, 4000, 400, 5600 };
+
+        //numArray2[6] = 1400;
+        //Console.WriteLine(numArray2[6]);
+        //Console.ReadLine();
+        }
+    }
+
+//IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+
+////1. Create a one-dimensional Array of strings.
+//Ask the user to select an index of the Array and then display the string at that index on the screen.
+
+//2. Create a one-dimensional Array of integers.
+//Ask the user to select an index of the Array and then display the integer at that index on the screen.
+
+//3. Add in a message that displays when the user selects an index that doesn’t exist.
+
+//4. Create a List of strings.Ask the user to select an index of the List and then display the content at that index on the screen.
