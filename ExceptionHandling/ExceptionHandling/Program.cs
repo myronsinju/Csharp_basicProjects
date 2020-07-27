7d8965a7f0ecd93ac35562241f49ed61b7739ca8
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-07-27 01:20:16 -0600

    exception handling assignment

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick another number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("dividing the two numbers...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("please type a whole number");
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Anything divided by 0 is itself");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }   
+    }
+}
