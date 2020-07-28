af81f0e78548c0686fa39ee4132c14e9b7aac69e
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-07-27 22:43:39 -0600

    hw

---
+++ class166/class166/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class166
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lets play with two numbers add one and display the other.");
+            int response = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("the math operation combined with.." + voidMethod.twoMethod(response));
+            Console.ReadLine();
+        }
+    }
+}
+//1. Create a class. In that class, create a void method that takes two integers as parameters.
+//    Have the method do a math operation on the first integer and display the second integer to the screen.
+
+//2. In the Main() method of the console app, instantiate the class.
+
+//3. Call the method in the class, passing in two numbers.
+
+//4. Call the method in the class, specifying the parameters by name
---
+++ class166/class166/voidMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class166
+{
+    class voidMethod
+    {
+        public static int twoMethod(int a = 5, int b = 0)
+        {
+            return a + b;
+        }
+    }
+}
