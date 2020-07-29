7ba8106805f865721a92922afbcc9e852ebd600f
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-07-29 17:37:01 -0600

    179

---
+++ class179/class179/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class179
+{
+    public class Employee : Person
+    {
+        public new string SayName { get; set; }
+    }
+}
