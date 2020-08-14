b6d454fa01f1279fe340c12fff1175e3f75cf4e9
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-08-14 03:43:17 -0600

    entity framework

---
+++ Class187/Class187/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class187
+{
+    class Employee : Person
+    {
+        public int Id { get; set; } 
+        public int Id2 { get; set; }
+    }
+    public virtual void list<ID> { get; set } 
+}
---
+++ Class187/Class187/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class187
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sample = new Employee()
+            {
+                FirstName = "sample",
+                LastName = "student",
+                Id = 01, 
+                Id2 = 01,
+            };
+            sample.SayName();
+            Console.ReadLine();
+        }
+    }
+}
+//1. Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
